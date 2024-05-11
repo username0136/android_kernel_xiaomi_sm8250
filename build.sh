dafd45e5efae2b2f9b59ee9faccbd9a1cb4bef07
Quick <quick@disroot.org>
2024-05-11 23:56:29 +0530

    fixup!: Cleanup and fixes

--- build.sh
+++ build.sh
@@ -66,7 +66,7 @@ KERVER=$(make kernelversion)
 COMMIT_HEAD=$(git log --oneline -1)
 
 # Date and Time
-DATE=$(TZ=Europe/Lisbon date +"%Y%m%d-%T")
+DATE=$(TZ=Asia/Kolkata date +"%Y%m%d-%T")
 TM=$(date +"%F%S")
 
 # Specify Final Zip Name
@@ -388,11 +388,11 @@ END=$(date +"%s")
 DIFF=$(($END - $START))
 move
 # KernelSU
-echo "CONFIG_KSU=y" >> $(pwd)/arch/arm64/configs/$DEFCONFIG
-compile_ksu
-move_ksu
+# echo "CONFIG_KSU=y" >> $(pwd)/arch/arm64/configs/$DEFCONFIG
+# compile_ksu
+# move_ksu
 zipping
-if [ "$BUILD" = "local" ]; then
+# if [ "$BUILD" = "local" ]; then
 # Discard KSU changes in defconfig
-git restore arch/arm64/configs/$DEFCONFIGf
-fi
+# git restore arch/arm64/configs/$DEFCONFIGf
+# fi
